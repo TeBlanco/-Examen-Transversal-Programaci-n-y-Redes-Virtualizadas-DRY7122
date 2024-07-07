e3365eeb3ed5eff7531ca5aa62d9d053665f71f4
TeBlanco <gio.cerda@duocuc.c>
2024-07-07 20:22:49 +0000

    Python Integrantes

---
+++ Integrantes_GDS.py
@@ -0,0 +1,9 @@
+integrantes = [
+    "Dennin Alicera",
+    "Giovanni Cerda",
+    "Simon Toro"
+]
+
+print("Nombres y Apellidos de los Integrantes del grupo:")
+for integrante in integrantes:
+    print(integrante)
